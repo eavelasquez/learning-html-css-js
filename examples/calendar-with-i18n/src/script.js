1d6787954dd9aea3905764b946f6f65739f17737
eavelasquez <eavc.30@gmail.com>
2023-06-13 19:11:28 -0500

    feat: complete calendar example

--- examples/calendar-with-i18n/src/script.js
+++ examples/calendar-with-i18n/src/script.js
@@ -1,42 +1,55 @@
-const locale = 'en'
-const intlMonth = new Intl.DateTimeFormat(locale, { month: 'long' })
-const intlWeekday = new Intl.DateTimeFormat(locale, { weekday: 'short' })
+const createCalendar = ({ locale = 'en', year = 2023 }) => {
+  const intlMonth = new Intl.DateTimeFormat(locale, { month: 'long' })
+  const intlWeekday = new Intl.DateTimeFormat(locale, { weekday: 'short' })
 
-const currentYear = new Date().getFullYear()
+  const weekdays = [...Array(7).keys()]
+  const weekdayNames = weekdays.map((day) => (
+    intlWeekday.format(new Date(2023, 0, day + 1))
+  ))
 
-const weekdays = [...Array(7).keys()]
-const weekdayNames = weekdays.map((day) => (
-  intlWeekday.format(new Date(2023, 0, day + 1))
-))
+  const months = [...Array(12).keys()]
 
-const months = [...Array(12).keys()]
+  const calendar = months.map((month) => {
+    const daysInMonth = new Date(year, month + 1, 0).getDate()
+    const daysOfMonth = [...Array(daysInMonth).keys()].map((day) => day + 1)
+    const firstDayOfMonth = new Date(year, month, 1).getDay()
+    const monthName = intlMonth.format(new Date(year, month))
 
-const calendar = months.map((month) => {
-  const daysInMonth = new Date(currentYear, month + 1, 0).getDate()
-  const daysOfMonth = [...Array(daysInMonth).keys()].map((day) => day + 1)
-  const firstDayOfMonth = new Date(currentYear, month, 1).getDay()
-  const monthName = intlMonth.format(new Date(currentYear, month))
+    return { daysOfMonth, firstDayOfMonth, monthName }
+  })
 
-  return { daysOfMonth, firstDayOfMonth, monthName }
-})
-
-const weekdayNamesHtml = weekdayNames.map((weekday) => (
-  `<li class="day-name">${weekday}</li>`
-)).join('')
-
-const monthsHtml = calendar.map(({ daysOfMonth, firstDayOfMonth, monthName }) => {
-  const monthHtml = `<h1>${monthName} ${currentYear}</h1>`
-
-  const firstDayOfMonthAttributes = `
-    class="first-day" style="--first-day-start: ${firstDayOfMonth}"
-  `
-  const weekdaysHtml = daysOfMonth.map((day, index) => (
-    `<li ${index === 0 ? firstDayOfMonthAttributes : ''}>${day}</li>`
+  const weekdayNamesHtml = weekdayNames.map((weekday) => (
+    `<li class="day-name">${weekday}</li>`
   )).join('')
 
-  const calendarHtml = `<ol>${weekdayNamesHtml} ${weekdaysHtml}</ol>`
+  const calendarHtml = calendar.map(({ daysOfMonth, firstDayOfMonth, monthName }) => {
+    const monthNameHtml = `<h2>${monthName} ${year}</h2>`
 
-  return `${monthHtml} ${calendarHtml}`
-}).join('')
+    const firstDayOfMonthAttributes = `
+    class="first-day" style="--first-day-start: ${firstDayOfMonth}"
+  `
+    const weekdaysHtml = daysOfMonth.map((day, index) => (
+      `<li ${index === 0 ? firstDayOfMonthAttributes : ''}>${day}</li>`
+    )).join('')
 
-document.querySelector('#calendar').innerHTML = monthsHtml
+    const monthHtml = `<ol>${weekdayNamesHtml} ${weekdaysHtml}</ol>`
+
+    return `<div class="month">${monthNameHtml} ${monthHtml}</div>`
+  }).join('')
+
+  document.querySelector('#calendar').innerHTML = calendarHtml
+
+  const el = document.querySelector('div')
+  const scrollHeight = window.innerHeight
+  document.querySelector('#next').addEventListener('click', () => {
+    el.scrollTo({ top: el.scrollTop + scrollHeight, behavior: 'smooth' })
+  })
+  document.querySelector('#previous').addEventListener('click', () => {
+    el.scrollTo({ top: el.scrollTop - scrollHeight, behavior: 'smooth' })
+  })
+}
+
+const locale = 'en'
+const currentYear = new Date().getFullYear()
+
+createCalendar({ locale, year: currentYear })
